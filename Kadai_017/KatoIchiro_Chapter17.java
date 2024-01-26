56afb5900100c0ec1fb21a7f4535cf39cb831b06
heavy-tank-bs <153990598+heavy-tank-bs@users.noreply.github.com>
2024-01-26 12:48:42 +0900

    Add files via upload

---
+++ Kadai_017/KatoIchiro_Chapter17.java
@@ -0,0 +1,10 @@
+package Kadai_017;
+
+public class KatoIchiro_Chapter17 extends Kato_Chapter17{
+	public void setGivenName() {
+		super.givenName = "一郎";
+	}
+	public void eachIntroduce() {
+		System.out.println("私の好きな食べ物はリンゴです");
+	}
+}
