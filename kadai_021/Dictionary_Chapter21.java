f95b4b7292a028d22c587ed2df4773896b8ce3f0
heavy-tank-bs <153990598+heavy-tank-bs@users.noreply.github.com>
2024-02-01 00:22:47 +0900

    Add files via upload

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,34 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+     public String dictionary(String word) {
+    	String wordMeans = null;
+		HashMap<String, String> dictionary = new HashMap<String, String>();
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "桃");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウイ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscat", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+		
+		for (String fruits: dictionary.keySet()) {
+			if (word == fruits) {
+				wordMeans = "の意味は" + fruits;
+				break;
+			}
+		}
+		
+		if (wordMeans == null) {
+			wordMeans = "は辞書に存在しません";
+		}
+		
+		return wordMeans;
+	} 
+}
